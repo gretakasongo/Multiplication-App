3511c4b4b0158379741222db886b44cb807270c2
gretakasongo <gretakasongo07@gmail.com>
2026-04-01 15:54:54 +0100

    Data on table display

--- app/src/main/java/com/example/multiplicationapp/MainActivity2.kt
+++ app/src/main/java/com/example/multiplicationapp/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.multiplicationapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,14 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+        // assigning data from intent to a variable
+        val bundle: Bundle? = intent.extras
+        // getting the table number from the intent
+        val tableString: String? = bundle?.getString("tableNumber")
+        // converting the table number to an integer
+        val tableNumber = tableString?.toInt()
+        val Table = findViewById<TextView>(R.id.Table)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
