a35567672584aee41e7e04da83ce7f3dbd3fce6f
gretakasongo <gretakasongo07@gmail.com>
2026-04-01 15:27:11 +0100

    Added comments

--- app/src/main/java/com/example/multiplicationapp/MainActivity.kt
+++ app/src/main/java/com/example/multiplicationapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiplicationapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,8 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // Created variables for the buttons and text input
         val multiplybtn = findViewById<Button>(R.id.multiplyBtn)
+        val numTxtInput = findViewById<EditText>(R.id.numTxtInput)
+        // Created an on click listener for the multiply button
         multiplybtn.setOnClickListener {
+            // Created an intent to go to the next activity
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
